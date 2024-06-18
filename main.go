1cbd2c14a450776a2b90e032b25d3996f57d9a66
YugenDev <yugoso8512@gmail.com>
2024-06-17 19:57:18 -0500

    get_by_ID

--- main.go
+++ main.go
@@ -17,19 +17,45 @@ var albums = []Album{
 	{ID: "1", Title: "Laterlus", Artist: "Tool", Year: 2001},
 	{ID: "2", Title: "Desire, I Want To Turn Into You", Artist: "Caroline Polachek", Year: 2023},
 	{ID: "3", Title: "Angel Dust", Artist: "Faith No More", Year: 1992},
-	{ID: "4", Title: "Slipknot", Artist: "Slipknot", Year: 1999},
+	{ID: "4", Title: "MIguel cacorro", Artist: "Slipknot", Year: 1999},
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+	var newAlbum Album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+
+	c.IndentedJSON(http.StatusCreated, albums)
+}
+
+func getAlbumByID(c *gin.Context){
+	id := c.Param("id")
+
+	for _, album := range albums{
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+		
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album no encontrado"})
+}
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/albums", func(ctx *gin.Context) {
-		getAlbums(ctx)
-	})
+	r.GET("/albums", getAlbums)
+	r.POST("/albums", postAlbums)
+	r.GET("/albums/:id", getAlbumByID)
 
 	r.Run()
 }
